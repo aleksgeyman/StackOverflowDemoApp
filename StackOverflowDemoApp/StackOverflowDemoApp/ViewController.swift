0f8b15b773dd3fc9e474c8493bd1dc9a138d9e1e
Aleksandar Geyman <a.geyman@tinqin.com>
2025-10-24 13:56:35 +0300

    create project

---
+++ StackOverflowDemoApp/StackOverflowDemoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StackOverflowDemoApp
+//
+//  Created by Aleksandar Geyman on 24.10.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
